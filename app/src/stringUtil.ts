1c6de9d5461200817927adfd5e51f6a44e67eb52
Barakat Ajadi <briannaajadi03@gmail.com>
2022-06-01 12:57:25 +0100

    update core-js

--- app/src/stringUtil.ts
+++
@@ -1,9 +0,0 @@
-
-export const truncateEnd = (text: string, maxLength: number): string => {
-  const diff = text.length - maxLength;
-  if (diff > 0) {
-    const start = text.substring(0, maxLength);
-    return `${start}...`;
-  }
-  return text;
-};
