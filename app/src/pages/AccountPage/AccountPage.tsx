80f881655a374571a5066f5c5d8eec9684304c12
Barakat Ajadi <briannaajadi03@gmail.com>
2022-06-20 09:19:48 +0100

    add loader

--- app/src/pages/AccountPage/AccountPage.tsx
+++ app/src/pages/AccountPage/AccountPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { RestMethod, truncateMiddle } from '@kibalabs/core';
 import { useStringRouteParam } from '@kibalabs/core-react';
-import { Alignment, Box, Direction, EqualGrid, Image, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
+import { Alignment, Box, Direction, EqualGrid, Image, LayerContainer, LoadingSpinner, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
 
 import { CollectionToken } from '../../client/resources';
 import { TokenCard } from '../../components/TokenCard';
@@ -55,26 +55,33 @@ export const AccountPage = (): React.ReactElement => {
   }, [getCollectionHoldings]);
 
   return (
-    <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} shouldAddGutters={true} paddingVertical={PaddingSize.Wide2}>
-      { accountAddress && (
-        <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true}>
-          <Box variant='rounded' shouldClipContent={true} height='40px' width='40px'>
-            <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${accountAddress}/image`} alternativeText='Avatar' />
-          </Box>
-          <Text variant='header2'>{truncateMiddle(accountAddress, 15)}</Text>
+    <LayerContainer>
+      <LayerContainer.Layer isFullHeight={true} isFullWidth={true}>
+        <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} shouldAddGutters={true} paddingVertical={PaddingSize.Wide2} isScrollableVertically={true}>
+          { accountAddress && (
+            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} isScrollableVertically={true}>
+              <Box variant='rounded' shouldClipContent={true} height='40px' width='40px'>
+                <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${accountAddress}/image`} alternativeText='Avatar' />
+              </Box>
+              <Text variant='header2'>{truncateMiddle(accountAddress, 15)}</Text>
+            </Stack>
+          )}
+          <Spacing variant={PaddingSize.Wide} />
+          <Stack.Item growthFactor={1}>
+            <EqualGrid childSizeResponsive={{ base: 6, medium: 6, large: 4, extraLarge: 3 }} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+              { holdings === undefined ? (
+                <LoadingSpinner />) : (
+                holdings && holdings.map((ownerToken: Token, index: number) : React.ReactElement => (
+                  <TokenCard
+                    key={index}
+                    token={ownerToken}
+                  />
+                )))}
+            </EqualGrid>
+          </Stack.Item>
         </Stack>
-      )}
-      <Spacing variant={PaddingSize.Wide} />
-      <Stack.Item growthFactor={1}>
-        <EqualGrid childSizeResponsive={{ base: 6, medium: 6, large: 4, extraLarge: 3 }} contentAlignment={Alignment.Start} shouldAddGutters={true} isFullHeight={false}>
-          {holdings && holdings.map((ownerToken: Token, index: number) : React.ReactElement => (
-            <TokenCard
-              key={index}
-              token={ownerToken}
-            />
-          ))}
-        </EqualGrid>
-      </Stack.Item>
-    </Stack>
+      </LayerContainer.Layer>
+    </LayerContainer>
+
   );
 };
