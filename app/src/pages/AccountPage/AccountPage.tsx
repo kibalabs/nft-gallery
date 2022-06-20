9c28e25b1340cc1ec6c5d7f054bb60026cd1963a
Barakat Ajadi <briannaajadi03@gmail.com>
2022-06-20 09:55:30 +0100

    fix scrolling

--- app/src/pages/AccountPage/AccountPage.tsx
+++ app/src/pages/AccountPage/AccountPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { RestMethod, truncateMiddle } from '@kibalabs/core';
 import { useStringRouteParam } from '@kibalabs/core-react';
-import { Alignment, Box, Direction, EqualGrid, Image, LayerContainer, LoadingSpinner, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
+import { Alignment, Box, Direction, EqualGrid, Image, LoadingSpinner, PaddingSize, Stack, Text } from '@kibalabs/ui-react';
 
 import { CollectionToken } from '../../client/resources';
 import { TokenCard } from '../../components/TokenCard';
@@ -55,33 +55,27 @@ export const AccountPage = (): React.ReactElement => {
   }, [getCollectionHoldings]);
 
   return (
-    <LayerContainer>
-      <LayerContainer.Layer isFullHeight={true} isFullWidth={true}>
-        <Stack direction={Direction.Vertical} isFullWidth={true} childAlignment={Alignment.Center} shouldAddGutters={true} paddingVertical={PaddingSize.Wide2} isScrollableVertically={true}>
-          { accountAddress && (
-            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} isScrollableVertically={true}>
-              <Box variant='rounded' shouldClipContent={true} height='40px' width='40px'>
-                <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${accountAddress}/image`} alternativeText='Avatar' />
-              </Box>
-              <Text variant='header2'>{truncateMiddle(accountAddress, 15)}</Text>
-            </Stack>
-          )}
-          <Spacing variant={PaddingSize.Wide} />
-          <Stack.Item growthFactor={1}>
-            <EqualGrid childSizeResponsive={{ base: 6, medium: 6, large: 4, extraLarge: 3 }} contentAlignment={Alignment.Center} shouldAddGutters={true}>
-              { holdings === undefined ? (
-                <LoadingSpinner />) : (
-                holdings && holdings.map((ownerToken: Token, index: number) : React.ReactElement => (
-                  <TokenCard
-                    key={index}
-                    token={ownerToken}
-                  />
-                )))}
-            </EqualGrid>
-          </Stack.Item>
+    <Stack direction={Direction.Vertical} isFullHeight={true} isFullWidth={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} paddingTop={PaddingSize.Wide}>
+      { accountAddress && (
+        <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} shouldAddGutters={true} paddingTop={PaddingSize.Wide2}>
+          <Box variant='rounded' shouldClipContent={true} height='40px' width='40px'>
+            <Image source={`https://web3-images-api.kibalabs.com/v1/accounts/${accountAddress}/image`} alternativeText='Avatar' />
+          </Box>
+          <Text variant='header2'>{truncateMiddle(accountAddress, 15)}</Text>
         </Stack>
-      </LayerContainer.Layer>
-    </LayerContainer>
-
+      )}
+      <Stack.Item growthFactor={1}>
+        <EqualGrid childSizeResponsive={{ base: 6, medium: 6, large: 4, extraLarge: 3 }} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+          { holdings === undefined ? (
+            <LoadingSpinner />) : (
+            holdings && holdings.map((ownerToken: Token, index: number) : React.ReactElement => (
+              <TokenCard
+                key={index}
+                token={ownerToken}
+              />
+            )))}
+        </EqualGrid>
+      </Stack.Item>
+    </Stack>
   );
 };
