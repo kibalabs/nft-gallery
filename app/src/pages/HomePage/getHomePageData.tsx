4b2fddaf81bef18441c8e3899a63ca0fbd0eae52
Krishan Patel <krishan711@gmail.com>
2022-07-20 12:05:36 +0100

    .

--- app/src/pages/HomePage/getHomePageData.tsx
+++ app/src/pages/HomePage/getHomePageData.tsx
@@ -25,8 +25,7 @@ export const getHomePageData = async (globals: IGlobals): Promise<IHomePageData>
   }
   // @ts-ignore
   // eslint-disable-next-line no-undef
-  const dataString = __non_webpack_require__(`./assets/${globals.projectId}/data.json`);
-  const data = JSON.parse(dataString);
+  const data = __non_webpack_require__(`./assets/${globals.projectId}/data.json`);
   const collection = Collection.fromObject(data.collection);
   const allTokens = data.collectionTokens.map((record: Record<string, unknown>): CollectionToken => CollectionToken.fromObject(record));
   const collectionAttributes = data.collectionAttributes.map((record: Record<string, unknown>): CollectionAttribute => CollectionAttribute.fromObject(record));
