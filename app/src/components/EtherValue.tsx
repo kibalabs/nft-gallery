bc7c70ab1ccc26afcce6c92b578ed356f096f799
Krishan Patel <krishan711@gmail.com>
2023-03-02 21:19:10 +0000

    .

--- app/src/components/EtherValue.tsx
+++ app/src/components/EtherValue.tsx
@@ -12,10 +12,6 @@ interface IEtherValueProps {
 
 export const EtherValue = (props: IEtherValueProps): React.ReactElement => {
   const textVariant = props.textVariant || 'default';
-  // const textTheme = useBuiltTheme('texts', textVariant);
-  const theme = useTheme();
-  const textTheme = theme.texts.default;
-  const height = textTheme['line-height'] || '1em';
   return (
     <Stack
       id={props.id}
@@ -23,7 +19,7 @@ export const EtherValue = (props: IEtherValueProps): React.ReactElement => {
       direction={Direction.Horizontal}
       childAlignment={Alignment.Center}
     >
-      <svg fill='currentColor' width={`calc(0.3 * ${height})`} height={`calc(0.6 * ${height})`} viewBox='0 0 14 26' xmlns='http://www.w3.org/2000/svg'>
+      <svg fill='currentColor' width={`calc(0.3 * 1em)`} height={`calc(0.6 * 1em)`} viewBox='0 0 14 26' xmlns='http://www.w3.org/2000/svg'>
         <path d='M7 0 14 13 7 18 0 13M0 15 7 26 14 15 7 20' />
       </svg>
       <Spacing variant={PaddingSize.Narrow} />
